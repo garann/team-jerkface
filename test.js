29cca2eaf8046ee721ec13a1b1fa32c3cc30e655
Clifton King <cliftonk@gmail.com>
2011-08-27 00:10:20 -0500

    many bros in test.js

---
+++ test.js
@@ -0,0 +1,37 @@
+require('coffee-script'); // dont hate
+
+var Game = require('./lib/game')
+  , game = new Game();
+
+game.join(null, 'broasaurus_rex', function (err, chan) {
+  game.join(chan.name, 'bronameth', function (err, chan) {
+    game.join(chan.name, 'bro', function (err, chan) {
+  
+    });
+  });
+  game.join(chan.name, 'bronameth', function (err, chan) {
+    game.join(chan.name, 'bro', function (err, chan) {
+  
+    });
+  });
+  game.join(chan.name, 'bronameth', function (err, chan) {
+    game.join(chan.name, 'bro', function (err, chan) {
+  
+    });
+  });
+  game.join(chan.name, 'bronameth', function (err, chan) {
+    game.join(chan.name, 'bro', function (err, chan) {
+  
+    });
+  });
+  game.join(chan.name, 'bronameth', function (err, chan) {
+    game.join(chan.name, 'bro', function (err, chan) {
+  
+    });
+  });
+  game.join(chan.name, 'bronameth', function (err, chan) {
+    game.join(chan.name, 'bro', function (err, chan) {
+  
+    });
+  });
+});

13d5795732acebba640b073fb2d46a88d8f5a710
Clifton King <cliftonk@gmail.com>
2011-08-26 21:05:29 -0500

    hello world, bro

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
-require('coffee-script');
 var http = require('http') 
   , nko = require('nko')('cx/IKz4MDPXythy2')
-  , $redis = require('./lib/adapter');
+  , everyauth = require('everyauth')
+  , $redis = require('./lib/adapter')
+  , connect = require('connect')
+  , everyauth = require('everyauth')
+  //, twitter = require('./lib/twitter')(everyauth)
+  , express = require('express')
+  , io = require('socket.io')
+  , config = require('./lib/config')
+  , users = {}
 
-var app = http.createServer(function (req, res) { 
-  res.writeHead(200, { 'Content-Type': 'text/html' }); 
-  res.end('Hello, World'); 
+everyauth
+  .twitter
+    .myHostname(config.host)
+    .consumerKey(config.twitter_key)
+    .consumerSecret(config.twitter_secret)
+    .findOrCreateUser(function (sess, accessToken, accessSecret, twitUser) {
+      return sess.uid = twitUser.screen_name;
+    })
+    .redirectPath('/');
+
+var app = express.createServer(
+    express.bodyParser()
+  , express.static(__dirname + "/public")
+  , express.cookieParser()
+  , express.session({ secret: 'really secret, bro'})
+  , everyauth.middleware()
+);
+
+io.listen(app);
+everyauth.helpExpress(app);
+app.configure(function () { app.set('view engine', 'jade'); });
+
+app.get('/', function (req, res) {
+  //if (req.session.uid) users[req.cookies['connect.sid']] = req.session.uid;
+  console.log(req.session)
+  res.render('home');
 });
 
-$redis.set('bro', 'what\'s up bro?', function (err) {
-  $redis.get('bro', function (err, resp) { console.log(resp); });
-});
-
-app.listen(parseInt(process.env.PORT) || 7777); 
+app.listen(config.port); 
 console.log('Listening on ' + app.address().port);

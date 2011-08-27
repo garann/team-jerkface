849ee697e37376b19092c0b027ad3c3e5d68175f
Clifton King <cliftonk@gmail.com>
2011-08-26 19:42:25 -0500

    hello world + redis test

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('coffee-script');
 var http = require('http') 
-  , nko = require('nko')('cx/IKz4MDPXythy2');
+  , nko = require('nko')('cx/IKz4MDPXythy2')
+  , $redis = require('./lib/adapter');
 
 var app = http.createServer(function (req, res) { 
   res.writeHead(200, { 'Content-Type': 'text/html' }); 
   res.end('Hello, World'); 
+});
+
+$redis.set('bro', 'what\'s up bro?', function (err) {
+  $redis.get('bro', function (err, resp) { console.log(resp); });
 });
 
 app.listen(parseInt(process.env.PORT) || 7777); 

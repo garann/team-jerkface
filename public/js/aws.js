698e3c7ff4dc3a125d0c8cb1915e646d0e1860d0
Garann Means <garann@gmail.com>
2011-08-26 20:31:02 -0500

    some events and tmpl stuff

--- public/js/aws.js
+++ public/js/aws.js
@@ -1,10 +1,23 @@
 var aws = {
+	var that = this,
+		$roster = $("#players"),
+		$game = $("#gameStage"),
+		$resp = $("#responseStage");
+
 	this.state = 0;
 	this.userInfo = {};
-	this.currentRound = {};
-	this.roomInfo = {};
+	this.currentRound = {
+		letters: null,
+		started: null,
+		length: null // get this from config
+	};
+	this.roomInfo = {
+		users: [],
+		round: 0
+	};
 
 	this.render = {
+		var rend = this;
 		
 		this.init = function() {
 			
@@ -18,7 +31,7 @@ var aws = {
 			
 		};
 
-		this.responseTimer = function() {
+		this.responseTimer = function(startTime, length) {
 			
 		};
 
@@ -26,57 +39,77 @@ var aws = {
 			
 		};
 
+		$.subscribe("rosterUpdated",function() {
+			$roster.html($.tmpl("rosterTmpl",that.roomInfo.users));
+		});
+
+		$.subscribe("roundStarted",function() {
+			$game.html($.tmpl("lettersTmpl",that.currentRound.letters;
+			$resp.html($.tmpl("responseTmpl",null));
+			rend.enableResponse();
+			rend.responseTimer(that.currentRound.started, that.currentRound.length);
+		});
+
 	};
 
 	this.sio = io.connect();
 	this.events = {
 		
 		sio.on("gameStarted", function(d) {
-			
+			// change state
 		});
 
 		sio.on("rosterUpdated", function(d) {
-			
+			// re-render roster
+			that.roomInfo.users = d.users;
+			$.publish("rosterUpdated");
 		});
 
 		sio.on("roundStarted", function(d) {
-			
+			// render letters
+			that.currentRound.letters = d.letters;
+			that.currentRound.started = Date.now();
+			// render response stage 
+			$.publish("roundStarted");
 		});
 
-		sio.on("responseSubmitted", function(d) {
+		//sio.on("responseSubmitted", function(d) {
 			
-		});
+		//});
 
 		sio.on("responseError", function(d) {
-			
+			// render error message
 		});
 
 		sio.on("roundEnded", function(d) {
-			
+			// destroy game stage
+			// destroy response stage
 		});
 
 		sio.on("votingStart", function(d) {
-			
+			// render response list
+			// wire-up voting controls
 		});
 
-		sio.on("voteSubmitted", function(d) {
+		//sio.on("voteSubmitted", function(d) {
 			
-		});
+		//});
 
 		sio.on("votingEnd", function(d) {
-			
+			// destroy response list
+			// destroy voting controls
 		});
 
 		sio.on("roundSummary", function(d) {
-			
+			// render responses
 		});
 
 		sio.on("gameEnded", function(d) {
-			
+			// render scores
 		});
 
 		sio.on("serverError", function(d) {
-			
+			// render error message
 		});
 
 	};
